afd24775c3ed091f354f1d99bb6a3a59e3d9606d
Vlad <vlad.jerez@gmail.com>
2022-12-01 13:39:40 -0400

    Comienzo del proyecto

---
+++ vuejs/01-BreakingBad/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <h1>App Vuejs</h1>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper{
+    display: flex;
+    flex-direction: column;
+  }
+  button{
+    cursor: pointer;
+  }
+</style>
